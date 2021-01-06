85b88d4ddb0b5d30669b6b885343bc28491560d9
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2021-01-05 19:38:14 -0600

    Starting the first OOP Project of 2021

---
+++ src/HotelMain.java
@@ -0,0 +1,8 @@
+public class HotelMain {
+
+    public static void Main(){
+        System.out.println("Welcome to Sonos Resort! " +
+                "How are you today!! What suite will you be interested in applying in?");
+    }
+
+}
